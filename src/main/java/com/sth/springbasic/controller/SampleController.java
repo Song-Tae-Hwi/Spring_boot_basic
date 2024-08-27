5cc9e121cbf089ea0b510785a319459ff34e623b
Song-Tae-Hwi <thdxognl4132@gmail.com>
2024-08-27 17:22:44 +0900

    Spring Basic

---
+++ src/main/java/com/sth/springbasic/controller/SampleController.java
@@ -0,0 +1,28 @@
+package com.sth.springbasic.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sth.springbasic.dto.PostSample1RequestDto;
+import com.sth.springbasic.servie.SampleService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/sample")
+@RequiredArgsConstructor
+public class SampleController {
+
+  private final SampleService sampleService;
+
+  @PostMapping("")
+  public ResponseEntity<String> postSample1(
+      @RequestBody @Valid PostSample1RequestDto requestBody) {
+    ResponseEntity<String> response = sampleService.postSample1(requestBody);
+    return response;
+  }
+}
---
+++ src/main/java/com/sth/springbasic/servie/SampleService.java
@@ -0,0 +1,9 @@
+package com.sth.springbasic.servie;
+
+import org.springframework.http.ResponseEntity;
+
+import com.sth.springbasic.dto.PostSample1RequestDto;
+
+public interface SampleService {
+  ResponseEntity<String> postSample1(PostSample1RequestDto dto);
+}
